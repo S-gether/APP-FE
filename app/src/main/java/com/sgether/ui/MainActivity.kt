23e415ae8909815365f4c6d12fdc038f06e858f2
Hong-Mu <hongmuchae@gmail.com>
2023-01-26 09:50:52 +0900

    refactor: 불필요 코드 삭제
    StudyRoomActivity 제거 및 RoomActivity로 대체

--- app/src/main/java/com/sgether/ui/MainActivity.kt
+++ app/src/main/java/com/sgether/ui/MainActivity.kt
@@ -1,31 +1,11 @@
 package com.sgether.ui
 
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import android.view.ViewTreeObserver
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.sgether.R
 import com.sgether.databinding.ActivityMainBinding
-import com.sgether.networks.AppSdpObserver
-import com.sgether.networks.PeerConnectionObserver
-import com.sgether.networks.PeerManager
-import com.sgether.networks.SocketManager
-import com.sgether.utils.PermissionHelper
-import io.socket.emitter.Emitter
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import org.json.JSONObject
-import org.webrtc.IceCandidate
-import org.webrtc.MediaStream
-import org.webrtc.SessionDescription
-import java.util.Timer
-import kotlin.concurrent.schedule
 
 class MainActivity : AppCompatActivity() {
 
@@ -37,7 +17,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         // Navigation
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
         val navController = navHostFragment.navController
         binding.bottomNavigationView.setupWithNavController(navController)
     }
