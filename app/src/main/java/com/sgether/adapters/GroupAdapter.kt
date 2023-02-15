425c197a3976ed857015adc9237d5e874162b5a6
rkdals0203 <rkdals0203@gmail.com>
2023-02-15 20:42:04 +0900

    feat - 230215 - MyGroup항목 스와이프하여 삭제 -close

--- app/src/main/java/com/sgether/adapters/GroupAdapter.kt
+++ app/src/main/java/com/sgether/adapters/GroupAdapter.kt
@@ -1,17 +1,28 @@
 package com.sgether.adapters
 
+import android.content.ClipData
 import android.content.Intent
+import android.graphics.Canvas
+import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.ItemTouchHelper.Callback.makeMovementFlags
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 import com.sgether.R
+import com.sgether.databinding.FragmentMyGroupBinding
 import com.sgether.databinding.ItemGroupBinding
 import com.sgether.networks.response.group.Room
 import com.sgether.ui.group.MyGroupFragmentDirections
 
-class GroupAdapter(var navController: NavController) : RecyclerView.Adapter<GroupAdapter.GroupViewHolder>() {
+class GroupAdapter(private val navController: NavController) :
+    RecyclerView.Adapter<GroupAdapter.GroupViewHolder>() {
 
     var list: List<Room> = listOf()
         set(value) {
@@ -19,21 +30,64 @@ class GroupAdapter(var navController: NavController) : RecyclerView.Adapter<Grou
             notifyDataSetChanged()
         }
 
-    inner class GroupViewHolder(val binding: ItemGroupBinding) :
+    inner class GroupViewHolder(private val binding: ItemGroupBinding) :
         RecyclerView.ViewHolder(binding.root) {
+
         fun bind(group: Room) {
             binding.textGroupName.text = group.room_name
             binding.textGroupInfo.text = group.created_at
 
             binding.root.setOnClickListener {
-                val action = MyGroupFragmentDirections.actionMyGroupFragmentToGroupInfoFragment(group.room_name?:"NULL")
+                val action =
+                    MyGroupFragmentDirections.actionMyGroupFragmentToGroupInfoFragment(
+                        group.room_name
+                            ?: "NULL"
+                    )
                 navController.navigate(action)
             }
         }
     }
 
+    inner class SwipeToDeleteCallback : ItemTouchHelper.Callback() {
+
+        override fun getMovementFlags(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder
+        ): Int {
+            val swipeFlags = ItemTouchHelper.START or ItemTouchHelper.END
+            return makeMovementFlags(0, swipeFlags)
+        }
+
+        override fun onMove(
+            recyclerView: RecyclerView,
+            viewHolder: RecyclerView.ViewHolder,
+            target: RecyclerView.ViewHolder
+        ): Boolean {
+            return false
+        }
+
+        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+            val position = viewHolder.adapterPosition
+            val item = list[position]
+
+            // delete the item from the adapter
+            list = list.toMutableList().apply { removeAt(position) }
+
+            // notify the adapter about the removed item
+            notifyItemRemoved(position)
+        }
+    }
+
+    val itemTouchHelper = ItemTouchHelper(SwipeToDeleteCallback())
+
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        itemTouchHelper.attachToRecyclerView(recyclerView)
+    }
+
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GroupViewHolder {
-        val binding = ItemGroupBinding.inflate(LayoutInflater.from(parent.context), parent, false);
+        val binding = ItemGroupBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return GroupViewHolder(binding)
     }
 
@@ -43,3 +97,4 @@ class GroupAdapter(var navController: NavController) : RecyclerView.Adapter<Grou
 
     override fun getItemCount() = list.size
 }
+
