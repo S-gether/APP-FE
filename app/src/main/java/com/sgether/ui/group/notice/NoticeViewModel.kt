7a93c3bab5797bcd252710468f03c9bd0a001182
rkdals0203 <rkdals0203@gmail.com>
2023-03-25 12:00:22 +0900

    feat - 230325 - DropGroup구현중 - close

--- app/src/main/java/com/sgether/ui/group/notice/NoticeViewModel.kt
+++ app/src/main/java/com/sgether/ui/group/notice/NoticeViewModel.kt
@@ -8,8 +8,8 @@ import com.sgether.model.Comment
 class NoticeViewModel: ViewModel() {
 
     private val _commentList = mutableListOf(
-        Comment("1","홍길동", "안녕21321313123"),
-        Comment("1","홍길동", "안녕213213"),
+        Comment("1","홍길동", "Hello guys! This is a notice board for Erpingchun Elementry school 3rd grade class 7"),
+        Comment("1","홍길동", "There was an error in the assignment on page 17 of the Korean language book today, so the teacher told me not to do it!"),
         Comment("1","홍길동", "안321313123"),
         Comment("1","홍길동", "안녕2133"),
         Comment("1","홍길동", "안녕2131313123"),
